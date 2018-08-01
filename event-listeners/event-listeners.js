53a897bf005a074720745a06323ae692ea906175
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:06:20 +0300

    stufffff

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,23 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['imgs/dog.jpg',"imgs/bb.jpeg","imgs/images.jpeg","imgs/images.jpg","imgs/jj.jpeg" ];
 var currentIndex = 0;
-
-function showNextPicture() {
-  currentIndex++; // increment current picture
+var currentimg = document.getElementsByTagName("img")[0]
+document.getElementById("im").addEventListener("click", function() {
+	currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
+  currentimg.src = pictures[currentIndex]
+});
 
-  // YOUR TASK: Finish this function!
-}
+
+// function showNextPicture() {
+//   currentIndex++; // increment current picture
+//   // if currentIndex is too large, start from the beginning again
+//   if (currentIndex >= pictures.length) {
+//     currentIndex = 0;
+//   }
+
+//   // YOUR TASK: Finish this function!
+// }
